19c08938565b0cddc001a963b48bb220de5076d7
abhi0287 <abhinav0287.ka@gmail.com>
2018-08-22 00:29:47 +0530

    dfgvdfv

--- Demo.java
+++ Demo.java
@@ -1,7 +1,8 @@
 public class Demo{
- public static void main(String[] x){
-     System.out.println("Hello World");
- }
+	public static void main(String[] x){
+		int a=5;
+		int b=10;
+                int c=a+b;
+		System.out.println(c);}
 }
-
-
+		
