b64764ef06183699132a4695ba4b5067cb3ed40e
abhi0287 <42529144+abhi0287@users.noreply.github.com>
2018-08-22 01:28:14 +0530

    Add files via upload

---
+++ Leap.java
@@ -0,0 +1,13 @@
+public class Leap{
+	public static void main(String[] n){
+	int x= 2016;
+	if(x%4==0)
+	{
+		System.out.println("this is leap year");
+	}
+	else
+	{
+		System.out.println("this is not leap year");
+	}
+	}
+	}
